ab25d2a7702ea3652ba91103281bf882bfb21cd8
Andreas Neumann <andr.neumann@googlemail.com>
2021-02-02 23:20:32 +0100

    shapefile-download integrated

--- run.sh
+++ run.sh
@@ -5,4 +5,24 @@ export PGPASSWORD=$POSTGRES_PASSWORD
 
 wget -O /tmp/data.osm.pbf $PBF_URL
 
-osm2pgsql --username $POSTGRES_USER --database $POSTGRES_DB --host $POSTGRES_HOST --port $POSTGRES_PORT --create --slim -G --hstore --tag-transform-script /usr/local/src/openstreetmap-carto/openstreetmap-carto.lua -C 2048 --number-processes ${THREADS:-4} -S /usr/local/src/openstreetmap-carto/openstreetmap-carto.style /tmp/data.osm.pbf
+# Import ShapeFiles
+python3 /usr/local/src/openstreetmap-carto/scripts/get-external-data.py --database "${POSTGRES_DB}" \
+                                                                        --host "${POSTGRES_HOST}" \
+                                                                        --port "${POSTGRES_PORT}" \
+                                                                        --username "${POSTGRES_USER}" \
+                                                                        --password "${POSTGRES_PASSWORD}"
+
+# Import PBF-File
+osm2pgsql --username "${POSTGRES_USER}" \
+          --database "${POSTGRES_DB}" \
+          --host "${POSTGRES_HOST}" \
+          --port "${POSTGRES_PORT}" \
+          --create \
+          --slim \
+          -G \
+          --hstore \
+          --tag-transform-script /usr/local/src/openstreetmap-carto/openstreetmap-carto.lua \
+          -C 2048 \
+          --number-processes "${THREADS:-4}" \
+          -S /usr/local/src/openstreetmap-carto/openstreetmap-carto.style \
+          /tmp/data.osm.pbf
